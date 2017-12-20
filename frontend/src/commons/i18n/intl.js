fbb64fc5738b2c2c6e886edad930f4ebcd4dd366
Guilherme Santos <sntguilherme@gmail.com>
2017-12-20 10:58:33 -0200

    refactor: more codes comments deleted

--- frontend/src/commons/i18n/intl.js
+++ frontend/src/commons/i18n/intl.js
@@ -7,49 +7,46 @@ import messages, {flattenMessages} from './messages';
 
 
 function configureLocale(renderCallback) {
-  addLocaleData(pt);
-  renderCallback();
+    addLocaleData(pt);
+    renderCallback();
 }
 
-/*
-* https://github.com/damonbauer/egghead-bookshelf/blob/lesson11-intl-polyfill/src/index.js
-*/
 export function configureReactIntlPolyfill(renderCallback) {
-  if (window.Intl) {
-    configureLocale(renderCallback);
-    return;
-  }
-
-  require.ensure([
-      'intl',
-      'intl/locale-data/jsonp/pt.js'
-    ],
-    require => {
-      require('intl');
-      require('intl/locale-data/jsonp/pt.js');
-
-      configureLocale(renderCallback);
+    if (window.Intl) {
+        configureLocale(renderCallback);
+        return;
     }
-  );
+
+    require.ensure([
+            'intl',
+            'intl/locale-data/jsonp/pt.js'
+        ],
+        require => {
+            require('intl');
+            require('intl/locale-data/jsonp/pt.js');
+
+            configureLocale(renderCallback);
+        }
+    );
 }
 
 export function getIntlProviderConfig() {
-  const locale = getLocale();
-  const messages = getFlattedLocaleMessages();
+    const locale = getLocale();
+    const messages = getFlattedLocaleMessages();
 
-  return {locale, messages};
+    return {locale, messages};
 }
 
 export function getFlattedLocaleMessages() {
-  let locale = getLocale();
-  return flattenMessages(messages[locale] || messages[DEFAULT_LOCALE]);
+    let locale = getLocale();
+    return flattenMessages(messages[locale] || messages[DEFAULT_LOCALE]);
 }
 
 export function getFormattedMessage(id) {
-  const config = getIntlProviderConfig();
+    const config = getIntlProviderConfig();
 
-  return new IntlProvider(config, {})
-    .getChildContext()
-    .intl
-    .formatMessage({id});
+    return new IntlProvider(config, {})
+        .getChildContext()
+        .intl
+        .formatMessage({id});
 }
