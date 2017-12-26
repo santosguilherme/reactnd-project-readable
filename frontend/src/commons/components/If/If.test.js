3d7860bffe25d6e8b9c83afff093987ef9d2c6c0
Guilherme Santos <sntguilherme@gmail.com>
2017-12-25 23:03:24 -0200

    chore: test configurations fix for macOS

--- frontend/src/commons/components/If/If.test.js
+++ frontend/src/commons/components/If/If.test.js
@@ -4,7 +4,7 @@ import {shallow} from 'enzyme';
 import If from './If';
 
 it('renders without crashing', () => {
-    const wrapper = shallow(<If/>);
+    const wrapper = shallow(<If><h1>test</h1></If>);
 
     expect(wrapper).toMatchSnapshot();
 });
@@ -12,6 +12,16 @@ it('renders without crashing', () => {
 it('Should render children when test property is true', () => {
     const wrapper = shallow(
         <If test={true}>
+            <span>Test</span>
+        </If>
+    );
+
+    expect(wrapper).toMatchSnapshot();
+});
+
+it('Shouldnt we render children when test property is false', () => {
+    const wrapper = shallow(
+        <If test={false}>
             <span>Test</span>
         </If>
     );
